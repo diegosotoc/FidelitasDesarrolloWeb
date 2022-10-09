4fe5a608a4f46d2110d43fc107bf6693fed81597
Diego Soto <juandisoca@gmail.com>
2022-10-08 19:57:35 -0600

    3 secciones listas, footer sticky en el bottom

--- Porfolio/index.js
+++ Porfolio/index.js
@@ -1,14 +1,11 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+// Funcion para mantener la navbar en el top de la pantalla 
 
-// Get the navbar
+window.onscroll = function () { stickyVar() };
+
 var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyVar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
