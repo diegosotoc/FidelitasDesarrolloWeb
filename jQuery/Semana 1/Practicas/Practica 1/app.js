4384c0112a3ef4ac81ca450d6d9695e7142ca2a5
Diego Soto <juandisoca@gmail.com>
2023-01-19 20:52:24 -0600

    Practica 1 terminada

---
+++ jQuery/Semana 1/Practicas/Practica 1/app.js
@@ -0,0 +1,27 @@
+$(".card").css({
+  "display": "flex",
+  "align-items": "center",
+  "justify-content": "center",
+  "flex-direction": "column",
+});
+
+$("img").css({
+  "width": "80%",
+  "height": "80%"
+});
+
+$("body").css({
+  "background": "#181a1b",
+});
+
+$("p").css({
+  "color": "#FFF",
+});
+
+$(".layaout").css({
+  "display": "flex",
+  "align-items": "center",
+  "justify-content": "center",
+  "flex-direction": "row",
+  "margin": "10px",
+});
